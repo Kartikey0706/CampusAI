e2a63666a3b0f1e9859ffa6b86ef0b4658fc6762
Kartikey Singh Suryavanshi <prajkartikey09@gmail.com>
2026-09-18 22:32:31 +0530

    initial commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,9 +12,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://campus-ai-gamma-liard.vercel.app","https://campus-ai-sepia.vercel.app"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://campus-ai-gamma-liard.vercel.app",
+        "https://campus-ai-sepia.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
