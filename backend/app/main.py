b92f07edf66f0d67738993ad123e12b61f28a2d4
Kartikey Singh Suryavanshi <prajkartikey09@gmail.com>
2026-09-18 19:32:06 +0530

    Update frontend CORS origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://campus-ai-gamma-liard.vercel.app"],
+    "https://campus-ai-gamma-liard.vercel.app","https://campus-ai-sepia.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
